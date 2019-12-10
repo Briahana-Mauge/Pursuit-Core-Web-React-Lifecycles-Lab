71b7ca4c1e24bce81359ec1f2d2c205cb0e3f6c2
Bpmauge <briahanamauge@pursuit.org>
2019-12-10 11:18:32 -0500

    added the todo list component with appropriate props

--- life-cycle-lab/src/App.js
+++ life-cycle-lab/src/App.js
@@ -2,28 +2,61 @@ import React from 'react';
 import './App.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import InputBox from './Components/InputBox';
+import ToDoList from './Components/todoList';
 
 toast.configure();
 class App extends React.Component {
   constructor(){
     super();
-    this.state = { test: 'test shown'}
+    this.state = { 
+      test: '',
+      listItems: [],
+      deleted: 'deleted'
+    }
+  }
+  componentDidMount() {
+    console.log(`app component mounted`);
+    
+  };
+  componentDidUpdate() {
+    console.log(`app component updated`);
   }
   handleTextChange = (event) => {
     this.setState({
       test: event.target.value
     })
   }
-  notify = () => {
-    let {test} = this.state
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let {test, listItems} = this.state;
     toast(test);
+
+    let newListItemsCopy = [...listItems];
+
+    newListItemsCopy.push({
+      item: test
+    })
+
+    this.setState({
+      listItems: newListItemsCopy
+    })
+    console.log(`new list`, newListItemsCopy, `old list`, listItems)
   }
+
   render(){
+    let {test, listItems} = this.state
   return (
     <div className="App">
-      <form onSubmit = {this.notify}>
-        <input type = 'text' onChange = {this.handleTextChange}></input>
-      </form>
+      <div className = 'input-stage'>
+      <InputBox submit={this.handleSubmit}
+            handleTextChange={this.handleTextChange}
+            test = {test}/>
+    </div>
+    <div className = 'list-stage'>
+      <ToDoList listItems = {listItems}
+      test = {test}/>
+    </div>
     </div>
   );
 }
