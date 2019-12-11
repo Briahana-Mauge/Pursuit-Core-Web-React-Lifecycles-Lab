cc723efaef7951e657f5966615f7651bc9e2d169
Bpmauge <briahanamauge@pursuit.org>
2019-12-11 14:13:36 -0500

    changed prop name to text for clarification

--- life-cycle-lab/src/Components/InputBox.jsx
+++ life-cycle-lab/src/Components/InputBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const InputBox = (props) => {
   return (
     <form onSubmit={props.submit}>
-      <input type='text' onChange={props.handleTextChange} placeholder='Enter A Todo'></input>
+      <input type='text' onChange={props.handleTextChange} placeholder='Enter A Todo' value = {props.text}></input>
     </form>
   )
 }
